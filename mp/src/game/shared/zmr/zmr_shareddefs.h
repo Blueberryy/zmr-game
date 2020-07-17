ba4c608acdc1831983b27ac6af8eb2e4be5535ea
TotallyMehis <totally.mehis@gmail.com>
2020-07-17 13:54:26 +0300

    Increase walk speed to match the anim speed

--- mp/src/game/shared/zmr/zmr_shareddefs.h
+++ mp/src/game/shared/zmr/zmr_shareddefs.h
@@ -158,7 +158,7 @@ enum ZMChatNotifyType_t
 
 #define ZM_MAX_MANI_DESC         256
 
-#define ZM_WALK_SPEED               190.0f
+#define ZM_WALK_SPEED               220.0f
 
 #define ZM_MIN_FOV          75 // Not the absolute minimum. GetMinFOV will not use this.
 
