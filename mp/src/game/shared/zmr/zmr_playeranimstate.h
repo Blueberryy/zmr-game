e4ae7f1e9c278881204176d0e9093c9818f2e2c6
TotallyMehis <totally.mehis@gmail.com>
2020-07-17 13:55:10 +0300

    Add basic support for 9way blending

--- mp/src/game/shared/zmr/zmr_playeranimstate.h
+++ mp/src/game/shared/zmr/zmr_playeranimstate.h
@@ -37,6 +37,8 @@ public:
 
 	virtual float GetCurrentMaxGroundSpeed() OVERRIDE;
 
+	bool Uses9WayAnim() const { return m_bIs9Way; }
+
 private:
 
 	bool            SetupPoseParameters( CStudioHdr* pStudioHdr );
@@ -70,4 +72,6 @@ private:
     float m_flCurrentHeadPitch;
     float m_flCurrentAimYaw;
     CountdownTimer m_blinkTimer;
+
+	bool m_bIs9Way;
 };
